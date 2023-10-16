71b7abede6ed51d9e28a2c2972dc1dc811f8baf0
sadiqueshahriar <shahriarshuvo714@gmail.com>
2023-10-16 14:06:38 +0600

    change admn url

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -29,7 +29,6 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Fortify::ignoreRoutes();
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
