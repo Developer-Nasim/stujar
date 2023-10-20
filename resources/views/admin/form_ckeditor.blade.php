90f6edb6e1ba239a96b4e6066265b05b32cddbb1
Developer-Nasim <ajnasim72@gmail.coom>
2023-10-20 13:18:25 +0600

    ck in edit notice

--- resources/views/admin/form_ckeditor.blade.php
+++ resources/views/admin/form_ckeditor.blade.php
@@ -1,23 +1,5 @@
-<script src="https://cdn.ckeditor.com/4.8.0/full-all/ckeditor.js"></script>
+<script src="//cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
 <div class="position-relative form-group">
     <label for="{{$name}}" class="">{{$label}}</label>
-    <textarea name="{{$name}}" id="editor" type="text" class="form-control">{{ $value ?? '' }}</textarea>
+    <textarea name="{{$name}}" class="ckeditor" type="text" class="form-control">{{ $value ?? '' }}</textarea>
 </div>
-<script>
-    CKEDITOR.replace('editor', {
-    skin: 'moono',
-    enterMode: CKEDITOR.ENTER_BR,
-    shiftEnterMode:CKEDITOR.ENTER_P,
-    toolbar: [{ name: 'basicstyles', groups: [ 'basicstyles' ], items: [ 'Bold', 'Italic', 'Underline', "-", 'TextColor', 'BGColor' ] },
-                { name: 'styles', items: [ 'Format', 'Font', 'FontSize' ] },
-                // { name: 'scripts', items: [ 'Subscript', 'Superscript' ] },
-                { name: 'justify', groups: [ 'blocks', 'align' ], items: [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
-                { name: 'paragraph', groups: [ 'list', 'indent' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent'] },
-                { name: 'links', items: [ 'Link', 'Unlink' ] },
-                { name: 'insert', items: [ 'Image'] },
-                { name: 'spell', items: [ 'jQuerySpellChecker' ] },
-                { name: 'table', items: [ 'Table' ] }
-                ],
-    });
-
-  </script>
--- resources/views/notice/edit.blade.php
+++ resources/views/notice/edit.blade.php
@@ -1,6 +1,7 @@
 @extends($websettings['cms_layout'].'.frontend.layouts.app')
 
 @section('content')
+
 <div class="container">
          <h2 class="text-center mt-4">Notice Edit</h2>      
         <div >
@@ -14,11 +15,30 @@
                     </label>
                     <label for="#">
                         Notice Details
-                        <textarea name="message">{{ $notice->message }}</textarea>
+                        <textarea name="message" id="editor">{{ $notice->message }}</textarea>
                     </label>
                     <button type="submit">Update</button>
                 </form>
             </div>
         </div>
 </div>
+<script src="https://cdn.ckeditor.com/4.8.0/full-all/ckeditor.js"></script>
+<script>
+    CKEDITOR.replace('editor', {
+    skin: 'moono',
+    enterMode: CKEDITOR.ENTER_BR,
+    shiftEnterMode:CKEDITOR.ENTER_P,
+    toolbar: [{ name: 'basicstyles', groups: [ 'basicstyles' ], items: [ 'Bold', 'Italic', 'Underline', "-", 'TextColor', 'BGColor' ] },
+                { name: 'styles', items: [ 'Format', 'Font', 'FontSize' ] },
+                // { name: 'scripts', items: [ 'Subscript', 'Superscript' ] },
+                { name: 'justify', groups: [ 'blocks', 'align' ], items: [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
+                { name: 'paragraph', groups: [ 'list', 'indent' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent'] },
+                { name: 'links', items: [ 'Link', 'Unlink' ] },
+                { name: 'insert', items: [ 'Image'] },
+                { name: 'spell', items: [ 'jQuerySpellChecker' ] },
+                { name: 'table', items: [ 'Table' ] }
+                ],
+    });
+
+  </script>
 @endsection
